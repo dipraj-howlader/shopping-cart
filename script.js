f5c44c2cf3493a8e1f5a8aaf6bf51ec65aef1b03
Dipraj Howlader <diprajhowlader2015@gmail.com>
2021-01-28 00:39:26 +0600

    calculation first part

--- script.js
+++ script.js
@@ -1,14 +1,21 @@
 const minusBtn = document.getElementById('minusBtn');
 minusBtn.addEventListener('click', function(){
     const value =  document.getElementById("minusValue").value;
-    const currentValue = value - 1;
+    const currentValue = parseFloat(value) - 1;
     document.getElementById("minusValue").value = currentValue;
+    const iphonePrice = document.getElementById("iphonePrice").innerText;
+    const totalResolved = parseInt(iphonePrice) - 1219;
+    document.getElementById('iphonePrice').innerText = totalResolved;
 
 })
 const plusBtn = document.getElementById('plusBtn');
 plusBtn.addEventListener('click', function(){
     const value = document.getElementById("minusValue").value;
-
     const currentValue =parseFloat(value) + 1;
     document.getElementById("minusValue").value = currentValue;
+
+    const iphonePrice = document.getElementById("iphonePrice").innerText;
+    const total = 1219 * currentValue;
+    document.getElementById('iphonePrice').innerText = total;
+
 })
