a34a3ff6b4b1fdec0531c8142469cf75ad9b855e
Dipraj Howlader <diprajhowlader2015@gmail.com>
2021-01-28 17:41:05 +0600

    half

--- script.js
+++ script.js
@@ -19,3 +19,28 @@ plusBtn.addEventListener('click', function(){
     document.getElementById('iphonePrice').innerText = total;
 
 })
+//  2nd part
+
+const CaseMinus = document.getElementById('CaseMinus');
+CaseMinus.addEventListener('click', function(){
+    handleProductChange(false);
+})
+const casePlus = document.getElementById('CasePlus');
+casePlus.addEventListener('click', function(){
+    handleProductChange(true);
+})
+
+function handleProductChange(isIncrease){
+    const caseInput = document.getElementById('changeOfValue');
+    const caseCount = parseInt(caseInput.value);
+    let caseNewCount = caseCount;
+    if(isIncrease == true ){
+        caseNewCount = caseCount + 1;
+    }
+    if(isIncrease == false && caseCount > 0){
+        caseNewCount = caseCount - 1;
+    }
+    caseInput.value = caseNewCount;
+    const caseTotal = caseNewCount * 59;
+    document.getElementById('case-total').innerText = caseTotal;
+}
