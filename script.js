6c6d5e193947fb7fea04cfe6d4fd01c2538d8374
Dipraj Howlader <diprajhowlader2015@gmail.com>
2021-01-28 18:13:20 +0600

    okokokoko

--- script.js
+++ script.js
@@ -1,24 +1,4 @@
-const minusBtn = document.getElementById('minusBtn');
-minusBtn.addEventListener('click', function(){
-    const value =  document.getElementById("minusValue").value;
-    const currentValue = parseFloat(value) - 1;
-    document.getElementById("minusValue").value = currentValue;
-    const iphonePrice = document.getElementById("iphonePrice").innerText;
-    const totalResolved = parseInt(iphonePrice) - 1219;
-    document.getElementById('iphonePrice').innerText = totalResolved;
 
-})
-const plusBtn = document.getElementById('plusBtn');
-plusBtn.addEventListener('click', function(){
-    const value = document.getElementById("minusValue").value;
-    const currentValue =parseFloat(value) + 1;
-    document.getElementById("minusValue").value = currentValue;
-
-    const iphonePrice = document.getElementById("iphonePrice").innerText;
-    const total = 1219 * currentValue;
-    document.getElementById('iphonePrice').innerText = total;
-
-})
 //  2nd part
 
 const CaseMinus = document.getElementById('CaseMinus');
@@ -43,4 +23,30 @@ function handleProductChange(isIncrease){
     caseInput.value = caseNewCount;
     const caseTotal = caseNewCount * 59;
     document.getElementById('case-total').innerText = caseTotal;
+}
+
+// 1st part
+
+const minusBtn = document.getElementById('minusBtn');
+minusBtn.addEventListener('click', function(){
+    firstProductChange(false);
+})
+const plusBtn = document.getElementById('plusBtn');
+plusBtn.addEventListener('click', function(){
+    firstProductChange(true);
+})
+
+function firstProductChange(isIncrease){
+    const caseInput = document.getElementById('minusValue');
+    const caseCount = parseInt(caseInput.value);
+    let caseNewCount = caseCount;
+    if(isIncrease == true ){
+        caseNewCount = caseCount + 1;
+    }
+    if(isIncrease == false && caseCount > 0){
+        caseNewCount = caseCount - 1;
+    }
+    caseInput.value = caseNewCount;
+    const caseTotal = caseNewCount * 1219;
+    document.getElementById('iphonePrice').innerText = caseTotal;
 }
