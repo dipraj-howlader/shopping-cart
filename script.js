8db0aa04dcbb63251e7bdd602aae0063798a7f50
Dipraj Howlader <diprajhowlader2015@gmail.com>
2021-01-29 21:16:03 +0600

    refresh

--- script.js
+++ script.js
@@ -1,15 +1,11 @@
-
-//  2nd part
-
-
-function handleProductChange(isIncrease){
-    const caseInput = document.getElementById('changeOfValue');
+function caseProductChange(isIncrease) {
+    const caseInput = document.getElementById('case-count');
     const caseCount = parseInt(caseInput.value);
     let caseNewCount = caseCount;
-    if(isIncrease == true ){
+    if (isIncrease == true) {
         caseNewCount = caseCount + 1;
     }
-    if(isIncrease == false && caseCount > 0){
+    if (isIncrease == false && caseCount > 0) {
         caseNewCount = caseCount - 1;
     }
     caseInput.value = caseNewCount;
@@ -17,20 +13,42 @@ function handleProductChange(isIncrease){
     document.getElementById('case-total').innerText = caseTotal;
 }
 
-// 1st part
 
 
-function firstProductChange(isIncrease){
-    const caseInput = document.getElementById('minusValue');
-    const caseCount = parseInt(caseInput.value);
-    let caseNewCount = caseCount;
-    if(isIncrease == true ){
-        caseNewCount = caseCount + 1;
-    }
-    if(isIncrease == false && caseCount > 0){
-        caseNewCount = caseCount - 1;
-    }
-    caseInput.value = caseNewCount;
-    const caseTotal = caseNewCount * 1219;
-    document.getElementById('iphonePrice').innerText = caseTotal;
-}
+
+
+// // 1st part
+
+
+// function phoneProductChange(isIncrease) {
+//     const phoneInput = document.getElementById('phone-count');
+//     const phoneCount = parseInt(phoneInput.value);
+//     let phoneNewCount = phoneCount;
+//     if (isIncrease == true) {
+//         phoneNewCount = phoneCount + 1;
+//     }
+//     if (isIncrease == false && phoneCount > 0) {
+//         phoneNewCount = phoneCount - 1;
+//     }
+//     phoneInput.value = phoneNewCount;
+//     const phoneTotal = phoneNewCount * 1219;
+//     document.getElementById('iphonePrice').innerText = phoneTotal;
+// }
+
+// //  2nd part
+
+
+// function caseProductChange(isIncrease) {
+//     const caseInput = document.getElementById('case-count');
+//     const caseCount = parseInt(caseInput.value);
+//     let caseNewCount = caseCount;
+//     if (isIncrease == true) {
+//         caseNewCount = caseCount + 1;
+//     }
+//     if (isIncrease == false && caseCount > 0) {
+//         caseNewCount = caseCount - 1;
+//     }
+//     caseInput.value = caseNewCount;
+//     const caseTotal = caseNewCount * 59;
+//     document.getElementById('case-total').innerText = caseTotal;
+// }
