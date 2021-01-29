4041b114f9c9dcb414c78ba6a3a8f54d3908948d
Dipraj Howlader <diprajhowlader2015@gmail.com>
2021-01-29 22:20:07 +0600

    all done

--- script.js
+++ script.js
@@ -1,20 +1,57 @@
-function caseProductChange(isIncrease) {
-    const caseInput = document.getElementById('case-count');
-    const caseCount = parseInt(caseInput.value);
-    let caseNewCount = caseCount;
+
+
+
+
+function caseProductChange(product, isIncrease) {
+
+    const productCount = getInputValue(product);
+    let productNewCount = productCount;
     if (isIncrease == true) {
-        caseNewCount = caseCount + 1;
+        productNewCount = productCount + 1;
     }
-    if (isIncrease == false && caseCount > 0) {
-        caseNewCount = caseCount - 1;
+    if (isIncrease == false && productCount > 0) {
+        productNewCount = productCount - 1;
     }
-    caseInput.value = caseNewCount;
-    const caseTotal = caseNewCount * 59;
-    document.getElementById('case-total').innerText = caseTotal;
+    document.getElementById(product + '-count').value = productNewCount;
+
+    let productTotal = 0;
+    if (product == 'phone') {
+        productTotal = productNewCount * 1219;
+    }
+    if (product == 'case') {
+        productTotal = productNewCount * 59;
+    }
+    document.getElementById(product + '-total').innerText = productTotal;
+
+    calculateTotal();
 }
 
+function calculateTotal(){
+   
+   const phoneCount = getInputValue('phone');
 
+   const caseCount = getInputValue('case');
 
+   const totalPrice = phoneCount * 1219 + caseCount * 59;
+
+   document.getElementById('total-price').innerText = '$' + totalPrice;
+
+   // tax 10%
+   const tax = Math.round(totalPrice*0.1);
+
+   document.getElementById('tax-amount').innerText = '$' + tax;
+
+   // grant total
+
+   const grandTotal = totalPrice + tax;
+   document.getElementById('grand-total').innerText = '$' + grandTotal;
+}
+
+function getInputValue(product){
+    const productInput = document.getElementById(product + '-count');
+   const productCount = parseInt(productInput.value);
+   return productCount;
+}
 
 
 // // 1st part
